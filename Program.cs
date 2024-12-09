36bb53fc0248c12e776b7bd061d075faa26501b1
Cantor Andrei <cantorandrei18@gmail.com>
2024-12-09 17:03:20 +0200

    add login and register using identity

--- Program.cs
+++ Program.cs
@@ -1,9 +1,16 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Proiect_MPA_EB_Cantor_Andrei.Data;
+using Microsoft.AspNetCore.Identity;
+
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddDbContext<Proiect_MPA_EB_Cantor_AndreiContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("Proiect_MPA_EB_Cantor_AndreiContext") ?? throw new InvalidOperationException("Connection string 'Proiect_MPA_EB_Cantor_AndreiContext' not found.")));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<IdentityContext>();
+
+builder.Services.AddDbContext<IdentityContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -23,10 +30,14 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
+
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapRazorPages();
 
 app.Run();
